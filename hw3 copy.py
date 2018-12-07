d0dc97815692552de86b73eb33a0a487d49c78d2
dogananil <doganmusa@itu.edu.tr>
2018-12-07 12:42:36 +0300

    Create Affine Matrix and do bilinear interpolation

--- hw3 copy.py
+++ hw3 copy.py
@@ -15,6 +15,7 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 import numpy as np
 import math
+from numpy.linalg import inv
 class App(QMainWindow):
     
     def __init__(self):
@@ -44,8 +45,11 @@ class App(QMainWindow):
         
         self.image = None
         self.image2 = None
+        self.triimage = None
+        self.triimage2 = None
         self.tmp_im = None
         self.imageLabel = None
+        self.imageLabel2 = None
         self.figure = Figure()
         self.figure2 = Figure()
         self.figure3 = Figure()
@@ -110,52 +114,142 @@ class App(QMainWindow):
     def draw_delaunay(self, subdiv ) :
         triangleList = subdiv.getTriangleList();
         size = self.image.shape
+        self.triimage = self.image
         r = (0, 0, size[1], size[0])
         for t in triangleList :
             pt1 = (t[0], t[1])
             pt2 = (t[2], t[3])
             pt3 = (t[4], t[5])
             if self.rectcontains(r, pt1) and self.rectcontains(r, pt2) and self.rectcontains(r, pt3) :
-                cv2.line(self.image, pt1, pt2, self.delaunay_color, 1, cv2.LINE_AA, 0)
-                cv2.line(self.image, pt2, pt3, self.delaunay_color, 1, cv2.LINE_AA, 0)
-                cv2.line(self.image, pt3, pt1, self.delaunay_color, 1, cv2.LINE_AA, 0)
+                cv2.line(self.triimage, pt1, pt2, self.delaunay_color, 1, cv2.LINE_AA, 0)
+                cv2.line(self.triimage, pt2, pt3, self.delaunay_color, 1, cv2.LINE_AA, 0)
+                cv2.line(self.triimage, pt3, pt1, self.delaunay_color, 1, cv2.LINE_AA, 0)
         
-        self.qImg = QImage(self.image.data,size[1],size[0],size[1]*3,QImage.Format_RGB888).rgbSwapped()
+        self.qImg = QImage(self.triimage.data,size[1],size[0],size[1]*3,QImage.Format_RGB888).rgbSwapped()
         
         self.imageLabel.setPixmap(QPixmap.fromImage(self.qImg))
         self.imageLabel.setAlignment(Qt.AlignCenter)
-        #self.inputBox.setLayout(imageLabel)
         self.inputBox.layout().addWidget(self.imageLabel)
     def draw_delaunay1(self, subdiv ) :
         triangleList = subdiv.getTriangleList();
         size = self.image2.shape
+        self.triimage2 =self.image2
         r = (0, 0, size[1], size[0])
+
         for t in triangleList :
             pt1 = (t[0], t[1])
             pt2 = (t[2], t[3])
             pt3 = (t[4], t[5])
-            for b in self.arnoldpoints:
-                temp = (b[1],b[0])
+            for b in range(0,len(self.bushpoints)):
+                temp = (self.bushpoints[b][1],self.bushpoints[b][0])
                 if(pt1==temp):
-                    a = temp
+                    a = (self.arnoldpoints[b][1],self.arnoldpoints[b][0])
                 if(pt2==temp):
-                    c = temp
+                    c = (self.arnoldpoints[b][1],self.arnoldpoints[b][0])
                 if(pt3==temp):
-                    d = temp
+                    d = (self.arnoldpoints[b][1],self.arnoldpoints[b][0]) 
+
             
             if self.rectcontains(r, pt1) and self.rectcontains(r, pt2) and self.rectcontains(r, pt3) :
+                ptt1 = pt1
+                ptt2 = pt2
+                ptt3 = pt3
                 pt1 = a
                 pt2 = c
                 pt3 = d
-                cv2.line(self.image2, pt1, pt2, self.delaunay_color, 1, cv2.LINE_AA, 0)
-                cv2.line(self.image2, pt2, pt3, self.delaunay_color, 1, cv2.LINE_AA, 0)
-                cv2.line(self.image2, pt3, pt1, self.delaunay_color, 1, cv2.LINE_AA, 0)
+                bmatrix1=[a[0],c[0],d[0]] # target image x's
+                mmatrix = [[ptt1[0],ptt1[1],1],[ptt2[0],ptt2[1],1],[ptt3[0],ptt3[1],1]]# input image 
+                bmatrix2 =[a[1],c[1],d[1]] # target image y's
+                minverse = inv(mmatrix)
+                amatrix1 = np.matmul(minverse,bmatrix1)
+                amatrix2 = np.matmul(minverse,bmatrix2)
+                amatrix = [[amatrix1[0],amatrix1[1],amatrix1[2]],[amatrix2[0],amatrix2[1],amatrix2[2]],[0,0,1]]
+                targettriangle = (pt1,pt2,pt3)
+                inputtriangle = (ptt1,ptt2,ptt3)
+                self.morphFunc(amatrix,targettriangle,inputtriangle)
+
+                cv2.line(self.triimage2, pt1, pt2, self.delaunay_color, 1, cv2.LINE_AA, 0)
+                cv2.line(self.triimage2, pt2, pt3, self.delaunay_color, 1, cv2.LINE_AA, 0)
+                cv2.line(self.triimage2, pt3, pt1, self.delaunay_color, 1, cv2.LINE_AA, 0)
         
-        self.qImg = QImage(self.image2.data,size[1],size[0],size[1]*3,QImage.Format_RGB888).rgbSwapped()
+        self.qImg = QImage(self.triimage2.data,size[1],size[0],size[1]*3,QImage.Format_RGB888).rgbSwapped()
         
         self.imageLabel1.setPixmap(QPixmap.fromImage(self.qImg))
         self.imageLabel1.setAlignment(Qt.AlignCenter)
         self.targetBox.layout().addWidget(self.imageLabel1)
+    def getbilinearpixel(self,posX,posY):
+        out = []
+        heightI,widthI,channelsI = self.image.shape      
+    	#Get integer and fractional parts of numbers
+        modXi = int(posX)
+        modYi = int(posY)
+        modXf = posX - modXi
+        modYf = posY - modYi
+        modXiPlusOneLim = min(modXi+1,self.image.shape[1]-1)
+        modYiPlusOneLim = min(modYi+1,self.image.shape[0]-1)
+     
+    	#Get pixels in four corners
+        for chan in range(self.image.shape[2]):
+            bl = self.image[modYi, modXi, chan]
+            br = self.image[modYi, modXiPlusOneLim, chan]
+            tl = self.image[modYiPlusOneLim, modXi, chan]
+            tr = self.image[modYiPlusOneLim, modXiPlusOneLim, chan]
+ 
+		#Calculate interpolation
+            b = modXf * br + (1. - modXf) * bl
+            t = modXf * tr + (1. - modXf) * tl
+            pxf = modYf * t + (1. - modYf) * b
+            out.append(int(pxf+0.5))
+        
+        return out  
+    def morphFunc(self,amatrix,targettriangle,inputtriangle):
+        heightI,widthI,channelsI = self.image.shape
+        invamatrix = inv(amatrix)
+        index = 0
+        coordt = []
+        for a in range(0,heightI):
+            for b in range(0,widthI):
+                bx=targettriangle[1][0]-targettriangle[0][0]
+                by=targettriangle[1][1]-targettriangle[0][1]
+                cx=targettriangle[2][0]-targettriangle[0][0]
+                cy=targettriangle[2][1]-targettriangle[0][1]
+                x=b-targettriangle[0][0]
+                y=a-targettriangle[0][1]
+                d=bx*cy-cx*by
+                wa = (x*(by-cy)+y*(cx-bx)+bx*cy-cx*by)/d
+                wb = (x*cy-y*cx)/d
+                wc = (y*bx-x*by)/d
+                if wa>0 and wa<1 and wb>0 and wb<1 and wc>0 and wc<1:
+                    coord = np.matmul(invamatrix,[b,a,1])
+                    coordinate = (coord[0],coord[1])
+                    coordt.append(coordinate)
+                    
+        for a in range(0,heightI):
+            for b in range(0,widthI):   
+                bix=inputtriangle[1][0]-inputtriangle[0][0]
+                biy=inputtriangle[1][1]-inputtriangle[0][1]
+                cix=inputtriangle[2][0]-inputtriangle[0][0]
+                ciy=inputtriangle[2][1]-inputtriangle[0][1]
+                xi=b-inputtriangle[0][0]
+                yi=a-inputtriangle[0][1]
+                di=bix*ciy-cix*biy
+                wai = (xi*(biy-ciy)+yi*(cix-bix)+bix*ciy-cix*biy)/di
+                wbi = (xi*ciy-yi*cix)/di
+                wci = (yi*bix-xi*biy)/di
+                if wai>0 and wai<1 and wbi>0 and wbi<1 and wci>0 and wci<1:
+                    if index<len(coordt):
+                        self.image[a][b]=self.getbilinearpixel(coordt[index][0],coordt[index][1])
+                        index=index+1
+        self.morphPrint()
+        
+    def morphPrint(self):
+        size = self.image.shape
+        self.qImg3 = QImage(self.image.data,size[1],size[0],size[1]*3,QImage.Format_RGB888).rgbSwapped()
+        
+        self.imageLabel2 = QLabel('Result')
+        self.imageLabel2.setPixmap(QPixmap.fromImage(self.qImg3))
+        self.imageLabel2.setAlignment(Qt.AlignCenter)
+        self.resultBox.layout().addWidget(self.imageLabel2)           
     def createTri(self):
         heightI,widthI,channelsI = self.image.shape
         self.red = self.image[:,:,2]
@@ -165,54 +259,23 @@ class App(QMainWindow):
         subdiv1 = cv2.Subdiv2D(rect1)
         rect2 = (0,0,widthI,heightI)
         subdiv2 = cv2.Subdiv2D(rect2)
-       # self.arnoldpoints = [[236,48],[174,54],[116,52],[98,92],[100,132],[194,192],[88,222],[160,242],[208,258],[200,290],[246,288],[282,274],[316,252],[360,228],[388,202],[408,150],[380,96],[348,70],[296,60],[210,62],[206,86],[206,100],[202,172],[202,196],[202,214],[208,224],[226,74],[229,95],[223,11],[217,193],[219,81],[219,173],[225,193],[219,211],[210,191],[296,141],[290,155],[292,165],[272,165],[294,119],[288,111],[268,119],[246,139],[210,135],[142,133],[336,101],[246,143],[328,187],[326,145],[326,115],[120,120],[264,214],[278,80],[282,48],[176,118],[178,154],[4,4],[14,308],[8,150],[472,4],[472,310],[462,150],[322,224]]
-       # self.bushpoints =   [[236,12],[282,22],[326,44],[366,66],[416,128],[402,204],[372,238],[312,268],[270,284],[214,286],[246,262],[272,256],[182,236],[146,238],[110,220],[92,204],[106,138],[102,102],[110,74],[120,52],[140,42],[168,42],[204,42],[232,38],[268,38],[242,20],[238,10],[274,16],[220,58],[196,108],[204,138],[198,162],[196,194],[202,216],[212,232],[238,70],[236,89],[234,105],[228,115],[220,103],[224,89],[230,77],[222,167],[226,189],[220,209],[212,191],[214,171],[282,145],[284,173],[242,143],[286,125],[330,109],[324,155],[322,197],[344,153],[138,131],[12,6],[14,308],[12,150],[474,6],[474,150],[474,306],[148,134]]
-       #self.arnoldpoints = [[4,4],[475,315],[240,315],[240,4],[4,315],[475,4],[4,160],[240,160],[475,160]]
         self.arnoldpoints = [[5,5],[5,315],[475,5],[475,315],[290,50],[238,52],[240,8],[348,66],[404,118],[396,190],[358,230],[288,264],[224,294],[200,286],[240,258],[174,240],[90,226],[100,132],[112,56],[202,82],[200,194],[224,92],[218,194],[142,136],[202,138],[246,136],[290,136],[284,168],[296,110],[334,102],[332,142],[328,188]]
         self.bushpoints = [[5,5],[5,315],[475,5],[475,315],[324,42],[260,38],[240,8],[360,60],[416,116],[410,200],[360,240],[310,262],[258,284],[216,282],[248,260],[184,236],[92,202],[106,128],[120,52],[206,84],[200,196],[230,98],[222,186],[142,132],[198,142],[244,142],[286,146],[282,178],[286,110],[334,100],[332,152],[326,200]]
         print("HeightI",heightI)
         print("Width",widthI)
-        for a in self.arnoldpoints:
+        for a in self.bushpoints:
             my_tuple = (a[1],a[0])
             subdiv1.insert(my_tuple)
             
-        for b in self.bushpoints:
+        for b in self.arnoldpoints:
             my_tuple1 = (b[1],b[0])
             subdiv2.insert(my_tuple1)
 
         
         self.draw_delaunay(subdiv1)
         self.draw_delaunay1(subdiv1)
-        #self.draw_delaunay(subdiv1) 
-    def morphFunc(self):
-        self.red = self.image[:,:,2]
-        self.green = self.image[:,:,1]
-        self.blue = self.image[:,:,0]
-        self.width,self.height = self.blue.shape
-        self.blueArray = [0]*256
-        self.redArray = [0]*256
-        self.greenArray = [0]*256
-        for w in range(0,self.width):
-            for h in range(0,self.height):
-                temp = self.blue[w][h]
-                self.blueArray[temp]+=1
-        for w in range(0,self.width):
-            for h in range(0,self.height):
-                temp = self.red[w][h]
-                self.redArray[temp]+=1        
-        for w in range(0,self.width):
-            for h in range(0,self.height):
-                temp = self.green[w][h]
-                self.greenArray[temp]+=1 
-        blueplot = self.figure.add_subplot(313)
-        redplot = self.figure.add_subplot(311)
-        greenplot = self.figure.add_subplot(312)
-        
-        blueplot.bar(range(256),self.blueArray,color = 'blue')
-        redplot.bar(range(256),self.redArray,color = 'red')
-        greenplot.bar(range(256),self.greenArray,color = 'green')
-        self.canvas.draw()
-        self.inputBox.layout().addWidget(self.canvas)
+    
+                    
     def open_Input(self):
         #fileName, _ = QFileDialog.getOpenFileName(self, "Open File",QDir.currentPath())
         fileName, _ = QFileDialog.getOpenFileName(self, 'Open Input', '.')
